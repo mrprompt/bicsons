77ac763d247a37e64394d64163012198ca41289e
Thiago Paes <mrprompt@gmail.com>
2017-11-14 18:50:56 -0200

    adicionando cliente da nexmo

--- README.md
+++ README.md
@@ -8,26 +8,33 @@ Este projeto foi criado para fins pessoais e didáticos apenas, **sem garantias*
 
 ## Instalação
 
-**Necessário Python 3**
-
-```
+```console
 pip install -r requirements.txt
-``` 
+```
 
 Crie um arquivo _.env_ na raiz do projeto e com seguintes campos:
 
-```
-API_KEY=<TOTALVOICE-API-KEY>
-NUMEROS="DDD+NUMERO"
+```console
+TOTALVOICE_API_KEY=<TOTALVOICE-API-KEY-VALUE>
+NEXMO_API_KEY=<NEXMO-API-KEY-VALUE>
+NEXMO_API_SECRET=<NEXMO-API-SECRET-VALUE>
+NUMEROS="DDDNUMERO,DDDNUMERO,DDDNUMERO,DDDNUMERO"
 ```
 
-* A api key, você pode conseguir [aqui](http://www.totalvoice.com.br/).
-* Os números devem seguir o formato DDD+NÚMERO, você pode inserir quantos números quiser, basta separá-los por vírgula. Mais detalhes, consulte a [documentação da API](https://api.totalvoice.com.br/doc/) da [TotalVoice](http://www.totalvoice.com.br/).
+* A api key da TotalVoice, você pode conseguir [aqui](http://www.totalvoice.com.br/).
+* A api key da Nexmo, você pode conseguir [aqui](https://www.nexmo.com/products/sms).
+* Os números devem seguir o formato DDDNÚMERO, você pode inserir quantos números quiser, basta separá-los por vírgula.
 
 ## Uso
 
-```
+```console
 python script.py
+```
+
+## Exemplo de crontab
+
+```console
+0 8,11,15,18,21 * * * echo 'cd /srv/app/bicsons; source venv/bin/activate; python script.py; deactivate' | /bin/bash >/dev/null 2>&1
 ```
 
 ## Licença
--- script.py
+++ script.py
@@ -5,31 +5,69 @@ from requests import get
 from dotenv import load_dotenv, find_dotenv
 import json
 import os
+import nexmo
+import random
 
 load_dotenv(find_dotenv(), override=True)
 
+numeros = os.environ.get("NUMEROS").split(",")
+
+"""
+Recupera a cotação para a Foxbit
+"""
 def bicsons():
     response = get('https://api.bitvalor.com/v1/ticker.json')
     result = json.loads(response.text)
 
     return json.dumps(result['ticker_1h']['exchanges']['FOX'])
 
+"""
+Envia o sms via TotalVoice
+"""
+def sms_totalvoice(numero, mensagem):
+    cliente = Cliente(os.environ.get("TOTALVOICE_API_KEY"), 'api.totalvoice.com.br')
+    response = cliente.sms.enviar(numero, mensagem)
+    result = json.loads(response.decode())    
+
+    return result
+
+"""
+Envia o sms via Nexmo
+"""
+def sms_nexmo(numero, mensagem):
+    cliente = nexmo.Client()
+    response = cliente.send_message({ 'from': 'Bicsons', 'to': '55' + numero, 'text': mensagem })
+
+    return { 'sucesso': bool(response['messages'][0]['status']) }
+
+"""
+Envia o sms com o driver escolhido aleatoriamente
+"""
 def sms(mensagem):
-    cliente = Cliente(os.environ.get("API_KEY"), 'api.totalvoice.com.br')
-    numeros = os.environ.get("NUMEROS").split(",")
     envios = 0;
 
-    for numero_destino in numeros:
-        response = cliente.sms.enviar(numero_destino, mensagem)
-        result = json.loads(response.decode())
+    for numero in numeros:
+        result = { 'sucesso' : True }
 
-        print("Envio para " + numero_destino + " : " + str(result['sucesso']))
+        driver = random.randint(1, 2)
+        
+        if driver == 1:
+            result = sms_totalvoice(numero, mensagem)
+        elif driver == 2:
+            result = sms_nexmo(numero, mensagem)
+        else:
+            print("sem driver apropriado para " + driver)
+
+        print("Envio para " + numero + " : " + str(result['sucesso']))
 
         if result['sucesso']:
             envios += 1
     
     return envios
 
+"""
+Função principal
+"""
 def main():
     sms(bicsons())
 
