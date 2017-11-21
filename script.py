59479c3eed5d33bee303e1ad7e090352415f04d8
Thiago Paes <mrprompt@gmail.com>
2017-11-21 15:59:57 -0200

    daemon test

--- script.py
+++ script.py
@@ -2,19 +2,25 @@
 Envia SMS da cotação do Bitcoin pela FoxBit
 """
 import json
+import time
 from dotenv import load_dotenv, find_dotenv
 from cotacao import bitcoin
 from sms import sms
+from daemons import daemonizer
 
 load_dotenv(find_dotenv(), override=True)
 
+@daemonizer.run(pidfile="/tmp/bicsons.pid")
 def main():
     """
     Função principal
     """
-    numeros = json.load(open("numeros.json"))
+    while True:
+        numeros = json.load(open("numeros.json"))
 
-    sms.sms(numeros, bitcoin.load('FOX'))
+        sms.sms(numeros, bitcoin.load('FOX'))
+
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
